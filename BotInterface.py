ed4c56a54727fbcd5b18b9caec89da733e3faf75
Kyle Young <kyoung24@urbanschool.org>
2023-09-30 10:49:36 -0700

    Bot Interfacing File

---
+++ BotInterface.py
@@ -0,0 +1,9 @@
+import discord
+
+bot = discord.Client()
+
+@bot.event
+async def on_message(message):
+    if message.content == "":
+
+#bot.run("MTE1NzcyNDY2NzMyNzU1MzY4Ng.GQvWKm.jTObRZxFGzWFU6-zbT0Wi-MvysfXyzYHnAmd5s")
