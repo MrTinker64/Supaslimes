4ae35cd4168ea29d518fb80e45413003342dc5a6
john <rpatel24@urbanschool.org>
2023-09-30 13:55:11 -0700

    f

--- Blackjack.py
+++ Blackjack.py
@@ -1,39 +1,4 @@
-#i had importing issues so i just copy and pasted everything from Deck.py
-import random
-
-class Card:
-    def __init__(self, suit, rank):
-        self.suit = suit
-        self.rank = rank
-
-    def __repr__(self):
-        return f"{self.rank} of {self.suit}"
-
-class Deck:
-    SUITS = ["Clubs", "Diamonds", "Hearts", "Spades"]
-    RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
-
-    def __init__(self):
-        self.cards = [Card(suit, rank) for suit in self.SUITS for rank in self.RANKS]
-
-    def resetDeck(self):
-        self.cards.clear()
-        self.cards = [Card(suit, rank) for suit in self.SUITS for rank in self.RANKS]
-
-    def shuffle(self):
-        random.shuffle(self.cards)
-
-    def draw(self, numberOfCards):
-        drawnCards = []
-        for i in range(numberOfCards):
-            if self.cards:
-                drawnCards.append(self.cards.pop())
-            else:
-                raise ValueError("Deck is empty!")
-        return drawnCards
-
-    def __repr__(self):
-        return f"Deck({self.cards})"
+from Deck import Deck
 
 #game code begins
 class Blackjack:
