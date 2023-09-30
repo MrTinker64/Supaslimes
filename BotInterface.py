4141e599d0cc11949c7b1680b7cbcb09bfbe156a
Kyle Young <kyoung24@urbanschool.org>
2023-09-30 11:41:02 -0700

    Bot interface updates

--- BotInterface.py
+++ BotInterface.py
@@ -1,9 +1,20 @@
 import discord
+import os
+import dotenv
+from discord.ext import commands
 
-bot = discord.Client()
+load_dotenv()
+TOKEN = os.getenv('MTE1NzcyNDY2NzMyNzU1MzY4Ng.GpAVLD.sKXn_29n700ELWV9tcVmTUOIdR6Jz-S0E-1Wdo')
+
+intents = discord.Intents(messages=True, guilds=True)
+
+
+
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_message(message):
-    if message.content == "":
+    if message.content == "$bridge":
+        await message.channel.send("hey dirtbag")
 
-#bot.run("MTE1NzcyNDY2NzMyNzU1MzY4Ng.GQvWKm.jTObRZxFGzWFU6-zbT0Wi-MvysfXyzYHnAmd5s")
+bot.run(TOKEN)
