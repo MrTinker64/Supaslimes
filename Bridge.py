e0081cc6d55484062c3fe3da30b35b81876ba705
William Coffer <wzcoffer@gmail.com>
2023-09-30 11:47:04 -0700

    made a function

--- Bridge.py
+++ Bridge.py
@@ -1,20 +1,22 @@
 from GamePlayers import BridgePlayer
 from Deck import Deck
 
-players = [
-    BridgePlayer(),
-    BridgePlayer(),
-    BridgePlayer(),
-    BridgePlayer(),
-]
+def runBridge():
+    players = [
+        BridgePlayer(),
+        BridgePlayer(),
+        BridgePlayer(),
+        BridgePlayer(),
+    ]
 
-teams = [[players[0], players[2]], [players[1], players[3]]]
+    teams = [[players[0], players[2]], [players[1], players[3]]]
 
-deck = Deck()
-deck.shuffle()
 
-for player in players:
-    player.receive_cards(deck.draw(13))
+    deck = Deck()
+    deck.shuffle()
 
-def Ai_turn(player):
-    return(random.choice(player_hand))
+    for player in players:
+        player.receive_cards(deck.draw(13))
+
+    def Ai_turn(player):
+        return(random.choice(player_hand))
