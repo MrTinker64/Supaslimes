4d73670b3d66bfdcfb62a2ac4e15873109bfdd26
MrTinker64 <avi.micah@gmail.com>
2023-09-30 11:04:27 -0700

    Working on player class

--- Player.py
+++ Player.py
@@ -1,2 +1,40 @@
 class Player:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.hand = []  # This will hold the player's cards.
+        self.points = 0
+        
+    def receive_card(self, card):
+        self.hand.append(card)
+        
+    def play_card(self, card):
+        if card in self.hand:
+            self.hand.remove(card)
+            return card
+        else:
+            raise ValueError(f"{card} not in player's hand!")
+        
+    def sort_hand(self):
+        # This assumes the Card class has a method to determine its order.
+        self.hand.sort(key=lambda card: (Deck.SUITS.index(card.suit), Deck.RANKS.index(card.rank)))
+        
+    def points_from_hand(self):
+        for card in self.hand:
+            if card.rank == "Ace":
+                self.points += 4
+            elif card.rank == "King":
+                self.points += 3
+            
+        
+    def __repr__(self):
+        return f"{self.name}, {self.points} points: {self.hand}"
+    
+if __name__ == "__main__":
+    deck = Deck()
+    player1 = Player("Avi")
+    deck.shuffle()
+    for _ in range(13):
+        player1.receive_card(deck.draw())
+    player1.sort_hand()
+    print(player1)
+        
