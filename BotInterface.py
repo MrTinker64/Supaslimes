57eb4510c1ea3c9b84f63fa58c2011680435603a
MrTinker64 <avi.micah@gmail.com>
2023-09-30 20:20:03 -0700

    Fixed duplicate issue

--- BotInterface.py
+++ BotInterface.py
@@ -6,8 +6,6 @@ from secretkey import TOKEN
 intents = discord.Intents(messages=True)
 client = discord.Client(intents=intents)
 
-intents.message_content = True
-
 @client.event
 async def discordPrint(message):
     # send message to user
