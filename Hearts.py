e4fbe0b5c095ff7085a744a5a80de453220b981b
MrTinker64 <avi.micah@gmail.com>
2023-09-30 19:30:14 -0700

    Finished Hearts without points

--- Hearts.py
+++ Hearts.py
@@ -16,7 +16,7 @@ for player in players:
     player.receive_cards(deck.draw(13))
     player.sort_hand()
     
-starting_player = players[random.randrange(4)]
+starting_player = players[0]
 
 def check_suit(cards, suit):
     list = []
@@ -24,6 +24,19 @@ def check_suit(cards, suit):
         if card.suit == suit:
             list.append(card)
     return list
+
+def count_points_for(player, trick):
+    for card in trick:
+        if card.suit == "Heart":
+            player.add_points(1)
+        if card.suit == "Spades" and card.rank == "Queen":
+            player.add_points(13)
+
+def get_highest_card(trick, lead_suit):
+    highest_card_value = max(card.numrank for card in trick if card.suit == lead_suit)
+    for card in trick:
+        if card.numrank == highest_card_value:
+            return card
 
 def trick(starting_index):
     trick = []
@@ -34,7 +47,10 @@ def trick(starting_index):
     reordered_players = players[starting_index:] + players[:starting_index]
 
     for player in reordered_players:
-        rank, of, suit = input(f"{player}, play a card: ").split()
+        # rank, of, suit = input(f"{player}, play a card: ").split()
+        played_card = player.hand[7]
+        rank = played_card.rank
+        suit = played_card.suit
         player.play_card(Card(suit, rank))
         trick.append(Card(suit, rank))
         if count == 1:
@@ -42,23 +58,17 @@ def trick(starting_index):
         count += 1
     
     print(trick)
-    trick = check_suit(trick, lead_suit)
-    highest_card_value = max(card.numrank for card in trick if card.suit == lead_suit)
-    highest_card = Card("Clubs", "2")
-    for card in trick:
-        if card.numrank == highest_card_value:
-            highest_card = card
+    suited_trick = check_suit(trick, lead_suit)
+    highest_card = get_highest_card(trick, lead_suit)
     print(f"{players[starting_index].name} started")
     player_to_win_trick = players[trick.index(highest_card)]
-    # player_to_win_trick.recieve_trick(trick)
-    print(trick)
+    print(suited_trick)
     print(f"{player_to_win_trick.name} won the trick")
     trick.clear
     count = 1
 
 trick(players.index(starting_player))
-# for player in players:
-#     player.count_points
-#     print(player.name, player.points)
+for player in players:
+    print(player.name, player.points)
     
 # of Spades
