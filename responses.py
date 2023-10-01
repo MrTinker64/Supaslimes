1d99c212954ad1cbb21397e37b7697c7fd653e4a
MrTinker64 <avi.micah@gmail.com>
2023-09-30 20:44:11 -0700

    testing responses.py

--- responses.py
+++ responses.py
@@ -2,22 +2,23 @@ from Bridge import runBridgeGame
 
 async def respond(msg):
     print("test")
-    new_message = msg.lower()
-    players_game = new_message.split()
+    # new_message = msg.lower()
+    new_message = "$bridge initialize game bridge a b c d"
+    split_message = new_message.split()
     #initializing game
 
     #test
-    bridgeGame = runBridgeGame(["1", "2", "3", "4"])
-    await bridgeGame.startGame()
+    # bridgeGame = runBridgeGame(["1", "2", "3", "4"])
+    # await bridgeGame.startGame()
 
-    if players_game[1] + players_game[2] == ('initialize game'):
-        if players_game[3] == 'bridge' or 'hearts' or 'blackjack' and len(players_game) == 8:
-            player1 = players_game[4]
-            player2 = players_game[5]
-            player3 = players_game[6]
-            player4 = players_game[7]
-            game = players_game[1]
-            return(f'{players_game[1]} Started! Good luck players!')
+    if split_message[1] + split_message[2] == ('initialize game'):
+        if split_message[3] == 'bridge' or 'hearts' or 'blackjack' and len(split_message) == 8:
+            player1 = split_message[4]
+            player2 = split_message[5]
+            player3 = split_message[6]
+            player4 = split_message[7]
+            game = split_message[3]
+            return(f'{split_message[3]} Started! Good luck players!')
     else:
         return("Please input a valid game and 4 players!")
 
